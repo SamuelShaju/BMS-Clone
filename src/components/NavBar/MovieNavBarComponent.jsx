84bb1f4fb72d99e437cf03bff647ed4b8d186a1c
SamuelShaju <samuel.shaju2020@vitstudent.ac.in>
2021-12-13 22:21:21 +0530

    final

--- src/components/NavBar/MovieNavBarComponent.jsx
+++ src/components/NavBar/MovieNavBarComponent.jsx
@@ -39,7 +39,9 @@ function NavLg() {
     <div className="container h-20 flex mx-auto max-w-none px-4 items-center justify-between">
         <div className="flex justify-centre w-1/2 gap-3">
             <div className="w-10 h-10">
-                <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="top-icon" className="w-full h-full rounded-md" />
+                <a href="/">
+                    <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="top-icon" className="w-full h-full rounded-md" />
+                </a>
             </div>
             <div className="w-full flex items-center gap-3 bg-white  px-3 py-1 rounded-md">
                 {/* <BiSearch/> */}
